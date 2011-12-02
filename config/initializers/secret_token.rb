318c8a4eaeb7b20ce01095d8b25d6f9d7c25ebf0
Kris-Mikael Krister <kris-mikael.krister@objectware.no>
2011-12-02 14:32:53 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Utgifter::Application.config.secret_token = 'bd25ede4738e5cfd8dd715ddc018e0079a7f4286586e67232ad9b51e4b0faceaa593d4a7f0db4146360451b7c4df47810aef2950bcd8a6d13835bbd8d355ccd8'
