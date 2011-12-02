0581f791dcf5bd0d3e5f1cbf7e792bd5740a6120
Kris-Mikael Krister <kris-mikael.krister@objectware.no>
2011-12-02 14:24:59 +0100

    starting from scratch

--- app/controllers/page_controller.rb
+++
@@ -1,7 +0,0 @@
-class PageController < ApplicationController
-  def show
-    @categories = Category.all
-    @expenses = Expense.all
-  end
-
-end
--- test/unit/helpers/expense_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ExpenseHelperTest < ActionView::TestCase
-end
