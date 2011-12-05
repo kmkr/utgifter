cefbd43257adc1e1586189190932e47e7389313f
Kris-Mikael Krister <kris-mikael.krister@objectware.no>
2011-12-05 11:23:42 +0100

    initial support for listing transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,10 @@
 class TransactionsController < ApplicationController
   respond_to :json
+  respond_to :html, :only => [ :index ]
+
+  def index
+    @transactions = Transaction.all
+  end
 
   def create
     @transaction = Transaction.create(params[:transaction])
