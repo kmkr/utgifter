7f62e1369ff30ee7b9d0ee6d3d5ac0ed9972cd66
Kris-Mikael Krister <krismikael@gmail.com>
2011-12-06 14:54:19 +0100

    parsing of real data

--- app/models/transaction_batch.rb
+++ app/models/transaction_batch.rb
@@ -26,18 +26,19 @@ class TransactionBatch < ActiveRecord::Base
   end
 
   def find_timestamp(batch_line)
-    # todo: regexp out tidspunkt
-     Time.new
+    # todo: kast exception dersom tidspunkt ikke finnes
+    batch_line.match(/\d{2}\.\d{2}\.\d{4}/).to_s.strip
   end
 
   def find_description(batch_line)
     # todo: regexp out beskrivelse
-    "description"
+    batch_line
   end
 
   def find_amount(batch_line)
-    # todo: regexp out beløp
-    10
+    line = batch_line.gsub(/\t/, "    ").chomp
+    match = line.match(/-?\d(\s?\d)*,\d{1,2}$/).to_s
+    match.sub(/,/, ".").gsub(/\s/, "").strip
   end
 
 end
--- test/unit/transaction_batch_test.rb
+++ test/unit/transaction_batch_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class TransactionBatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "correct creation of withdrawal" do
+    tb = TransactionBatch.new
+    tb.content = "28.11.2011  VARER: 27.11 RESTAURANT DAHL MARIDALSVN 4 OSLO  28.11.2011  -600,00"
+    
+    transactions = tb.convert_to_transactions
+
+    assert_equal(1, transactions.size)
+
+    transaction = transactions.first
+    assert_equal(tb.content, transaction.description)
+    assert_equal(-600, transaction.amount)
+    assert_equal(2011, transaction.time.year)
+    assert_equal(28, transaction.time.day)
+    assert_equal(11, transaction.time.month)
+  end
+
+  test "correct creation of withdrawal above one thousand kroner" do
+    tb = TransactionBatch.new
+    tb.content = "29.11.2011  MINIBANK-UTTAK I FREMMED BANK: 28.11 BNP Alexander Kiellands 0171 Oslo  28.11.2011  -1 400,00"
+    
+    transactions = tb.convert_to_transactions
+
+    assert_equal(1, transactions.size)
+
+    transaction = transactions.first
+    assert_equal(tb.content, transaction.description)
+    assert_equal(-1400, transaction.amount)
+    assert_equal(2011, transaction.time.year)
+    assert_equal(29, transaction.time.day)
+    assert_equal(11, transaction.time.month)
+  end
+
+  test "correct creation of withdrawal above one million kroner" do
+    tb = TransactionBatch.new
+    tb.content = "29.11.2011  MINIBANK-UTTAK I FREMMED BANK: 28.11 BNP Alexander Kiellands 0171 Oslo  28.11.2011  -1 230 400,00"
+    
+    transactions = tb.convert_to_transactions
+    transaction = transactions.first
+    assert_equal(-1230400, transaction.amount)
+  end
+
+  test "correct creation of wage with tab" do
+    tb = TransactionBatch.new
+    tb.content = "22.11.2011  OVERFORSEL: Fra: ITERA CONSULTING AS Betalt: 21.11.11 22.11.2011	1 281,55"
+    
+    transactions = tb.convert_to_transactions
+
+    assert_equal(1, transactions.size)
+
+    transaction = transactions.first
+    assert_equal(1281.55, transaction.amount)
+    assert_equal(2011, transaction.time.year)
+    assert_equal(22, transaction.time.day)
+    assert_equal(11, transaction.time.month)
+  end
+
 end
